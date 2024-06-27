7cfb9542f204f554eb2ab48b8705b5190f47abc3
Akansha Pandey <127675854+AkanshaP2@users.noreply.github.com>
2024-06-27 11:56:15 +0530

    Add files via upload

---
+++ images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p images
+cd images
+
+# Download files from GitHub
+wget https://github.com/AkanshaP2/images/raw/main/image1.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image2.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image3.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image4.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image5.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image6.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image7.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image8.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image9.jpeg
+wget https://github.com/AkanshaP2/images/raw/main/image10.jpeg
+
+
+
+
